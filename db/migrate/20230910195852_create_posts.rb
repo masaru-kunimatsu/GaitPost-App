063bff50ad235e57e20a365fda7298942c94a5b3
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-09-12 05:35:02 +0900

    devise導入

---
+++ db/migrate/20230910195852_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :detail, null: false
+      t.text :literature, null: false
+      t.integer :cycle_id, null: false
+      t.integer :muscle_id
+      t.integer :joint_id
+      t.integer :neuron_id
+      #t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
