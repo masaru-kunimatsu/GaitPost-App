a0c7a41c6efd82de01e94f912448c4eef70f273d
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-09-15 05:08:31 +0900

    詳細ページ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all 
   end
+
+  def new
+    @posts = Post.new
+  end
+
+  def show
+    
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to action: :show
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :detail, :literature, :cycle_id, :muscle_id, :joint_id, :neuron_id).merge(user_id: current_user.id)
+  end
+
 end
+
+
+
+
