81155efdeb1bb7c8fd03194142fb83f846c4ae43
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-09-17 22:35:06 +0900

    新規投稿ページ修正

--- app/models/walkcycle.rb
+++ app/models/walkcycle.rb
@@ -1,14 +1,14 @@
 class Walkcycle < ActiveHash::Base
    extend ActiveHash::Associations::ActiveRecordExtensions 
   self.data = [
-    { id: 1, name: 'IC' },
-    { id: 2, name: 'LR' },
-    { id: 3, name: 'MSt' },
-    { id: 4, name: 'TSt' },
-    { id: 5, name: 'PSw' },
-    { id: 6, name: 'ISw' },
-    { id: 7, name: 'MSw' },
-    { id: 8, name: 'TSw' },
+    { id: 1, name: 'IC ～ LR' },
+    { id: 2, name: 'LR ～ MSt' },
+    { id: 3, name: 'MSt ～ TSt' },
+    { id: 4, name: 'TSt ～ PSw' },
+    { id: 5, name: 'PSw ～ ISw' },
+    { id: 6, name: 'ISw ～ MSw' },
+    { id: 7, name: 'MSw ～ TSw' },
+    { id: 8, name: 'TSw ～ IC' },
   ]
 
   include ActiveHash::Associations
