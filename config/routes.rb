27ea17a6fcb9ee1713a804461855ca669ae1b86b
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-09-18 03:18:02 +0900

    インクリメンタルサーチの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :users, only: [:show]
-  resources :posts
+  resources :posts do
+    collection do
+      get 'search'
+    end
+  end
 end
