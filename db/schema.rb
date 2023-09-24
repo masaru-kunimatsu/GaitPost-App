b0f1a5d9e2a08bb39bddf928be26b9dc50062324
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-09-25 00:07:01 +0900

    migration順番変更

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2023_09_12_203440) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
