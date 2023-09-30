22e023cc3fca0935859b5b3a6ae677d0d04c1c18
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-10-01 03:25:53 +0900

    READMEsyuusei

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    get :likes 
+  end
   resources :posts do
     collection do
       get 'search'
