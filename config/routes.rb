ebcf751a090683e21d796831f137a18f8c79c5c3
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-10-01 18:09:30 +0900

    いいねした投稿を表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :users, only: [:show] do
-    get :likes 
-  end
+  resources :users, only: [:show]
   resources :posts do
     collection do
       get 'search'
-      get 'search_post'
+      get 'search_post' 
     end
-    resource :likes, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
     resources :comments, only: :create
   end
 end
