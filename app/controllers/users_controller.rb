8c97f87a3666aa4505a5675bc0118a7db5289220
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-10-05 02:46:50 +0900

    user詳細ページでログイン分岐

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
   def show
     user = User.find(params[:id])
     @posts = current_user.posts.order(created_at: :desc)
