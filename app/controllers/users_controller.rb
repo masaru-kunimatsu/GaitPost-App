9a13e8b3136ccd2175d5f5d2a61ca8e395c8cb08
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-10-07 22:19:59 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def show
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
     @posts = current_user.posts.order(created_at: :desc)
     @comment_posts = Post.includes(:comments)
       .where(comments: { user_id: current_user.id })
